20d3290a8878d9bd9ecb8e00ebb4bea9d0033810
KIM Kangseong <KIM.Kangseong@gmail.com>
2019-01-20 15:18:46 +0900

    [Finishes #162837601] Implement sider animation
        - And update sider style

--- src/views/sider/config.jsx
+++ src/views/sider/config.jsx
@@ -9,7 +9,9 @@ import './config.less';
 const ConfigItem = ({label, checked, onChange}) => (
   <div className='it-config-item'>
     <label>{label}</label>
-    <Checkbox checked={checked} primary={true} onChange={onChange} />
+    <div>
+      <Checkbox checked={checked} primary={true} onChange={onChange} />
+    </div>
   </div>
 );
 
@@ -31,17 +33,17 @@ class SiderConfig extends Component {
     return (
       <Sider title='Settings' ref={r => this._siderConfig = r}>
         <ConfigItem
-            label='시간 초과 시 알람 소리'
+            label='Alarm sound'
             checked={ringable}
             onChange={toggleRingable}
             />
         <ConfigItem
-            label='시간 초과 시 숫자 흔들림'
+            label='Alarm shaking animation'
             checked={animatable}
             onChange={toggleAnimatable}
             />
         <ConfigItem
-            label='배경색 채움'
+            label='Fill background'
             checked={filled}
             onChange={toggleFilled}
             />
